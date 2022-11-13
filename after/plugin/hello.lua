72e04ad753b8bd11d0afae695563a9066b6e141d
scubass <santiagocubas01@gmail.com>
2022-11-13 19:11:49 -0300

    --

--- after/plugin/hello.lua
+++ after/plugin/hello.lua
@@ -2,4 +2,6 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-keymap("", "<leader>e", "<cmd>lua print(\"Hello\")<cr>", opts)
+keymap("n", "<leader>e", "<cmd>lua print(\"Hello\")<cr>", opts)
+
+print("Hello from a lua file")
